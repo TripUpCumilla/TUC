062d3883122ea322b5f69172521216ee4fe5d363
TripUpCumilla <farokm910@gmail.com>
2025-11-10 14:06:30 +0600

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ window.addEventListener('load', function() {
 function closePopup() {
   document.getElementById('popup').style.display = 'none';
 }
+
+
+// Open Popup
+function openAuth() {
+  document.getElementById("authPopup").style.display = "flex";
+}
+
+// Close Popup
+function closeAuth() {
+  document.getElementById("authPopup").style.display = "none";
+}
+
+// Tab Change System
+function openTab(tabId) {
+
+  // Hide all forms
+  document.querySelectorAll(".auth-form").forEach(form => {
+    form.classList.add("hidden");
+  });
+
+  // Remove active class from tabs
+  document.querySelectorAll(".tab-btn").forEach(btn => {
+    btn.classList.remove("active");
+  });
+
+  // Show selected tab
+  document.getElementById(tabId).classList.remove("hidden");
+
+  // Add active to clicked button
+  event.target.classList.add("active");
+}
